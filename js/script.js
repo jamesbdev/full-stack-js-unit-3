2afa690b2bbea9fdf8a92ae53e40737a81ca1707
James Bremner <j.bremner@ymail.com>
2024-02-26 12:20:48 +0000

    add js file

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const nameInput = document.querySelector("#name");
+
+nameInput.focus();
+
+const otherJobInput = document.querySelector("#other-job-role");
+//hide the other job input field by default
+otherJobInput.setAttribute("hidden", "");
+
+
+
+
