23b0cdea1847e6ff49a197fef0a7b0badb388bf9
James Bremner <j.bremner@ymail.com>
2024-02-26 14:35:27 +0000

    show/hide text input

--- js/script.js
+++ js/script.js
@@ -2,9 +2,30 @@ const nameInput = document.querySelector("#name");
 
 nameInput.focus();
 
-const otherJobInput = document.querySelector("#other-job-role");
-//hide the other job input field by default
-otherJobInput.setAttribute("hidden", "");
+const displayTextField = () => {
+    const otherJobInput = document.querySelector("#other-job-role");
+    const jobRole = document.querySelector("#title");
+    //hide the other job input field by default
+    otherJobInput.setAttribute("hidden", "");
+
+    jobRole.addEventListener("change", (event) => {
+       const options = event.target.querySelectorAll("option");
+       //check if the selected option is "other"
+       //if yes show the text input
+       options.forEach(option => {
+         if(option.selected == true && option.value.toLowerCase() == "other") {
+            //display the text input 
+            otherJobInput.removeAttribute("hidden");
+         } else {
+            otherJobInput.setAttribute("hidden", "");
+         }
+       });
+    })
+
+}
+
+displayTextField();
+
 
 
 
